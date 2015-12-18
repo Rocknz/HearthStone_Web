6b1f39b0c6328ad3f6e6de8db87c81e195aa4bfe
정 락준 <jrj325@hanmir.com>
2015-12-18 09:46:12 +0900

    This is just test commit for my AWS

--- WebPage/admin.py
+++ WebPage/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None,               {'fields': ['question_text']}),
+                ('Date information', {'fields': ['pub_date']})]
+    list_display = ('question_text', 'pub_date')
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
--- WebPage/views.py
+++ WebPage/views.py
@@ -1,21 +1,40 @@
-from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from .models import Question, Choice
 import datetime
 
 
 def index(request):
-    name = datetime.datetime.now()
-    return HttpResponse(name)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'detail.html', {'question': question})
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('results', args=(question.id,)))
 # Create your views here.
